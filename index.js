7863dae398bfa1ccfa7e6b382f987e7304b5e22e
Yousef <yousefsameh24@gmail.com>
2023-07-08 22:01:42 +0300

    solve 3.4

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    phonebook = phonebook.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
