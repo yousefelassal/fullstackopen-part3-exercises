7b908d7b89766ee39951624289a5232f0f6d91ba
Yousef <yousefsameh24@gmail.com>
2023-07-09 22:00:39 +0300

    solve 3.11

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
