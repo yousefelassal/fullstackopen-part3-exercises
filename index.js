4ae8379c543c0d8c3540aaf0c5abd1b47e4c8fa0
Yousef <yousefsameh24@gmail.com>
2023-07-09 21:22:00 +0300

    solve 3.9

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
