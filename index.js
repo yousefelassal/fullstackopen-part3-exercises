00171c81464e06d319570dc66510c3d05b184cca
Yousef <yousefsameh24@gmail.com>
2023-07-11 16:53:22 +0300

    solve 3.18

--- index.js
+++ index.js
@@ -12,29 +12,6 @@ app.use(express.static('build'));
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let phonebook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (req, res) => {
     Phonebook.find({}).then(phonebook => {
         res.json(phonebook)
@@ -43,7 +20,9 @@ app.get('/api/persons', (req, res) => {
 
 app.get('/info', (req, res) => {
     const date = new Date()
-    res.send(`<p>Phonebook has info for ${phonebook.length} people</p><p>${date}</p>`)
+    Phonebook.find({}).then(phonebook => {
+        res.send(`<p>Phonebook has info for ${phonebook.length} people</p><p>${date}</p>`)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
